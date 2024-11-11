f65a7e4cefb4b27add786a6c7ca84b7606b4eed6
c-abhijith <cabhijithofficial@gmail.com>
2024-11-11 22:41:51 +0530

    host is added

--- ChatApp/urls.py
+++ ChatApp/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.CreateRoom, name='create-room'),
-    path('<str:room_name>/<str:username>/', views.MessageView, name='room'),
+    path('ws/<str:room_name>/<str:username>/', views.MessageView, name='room'),
 ]
